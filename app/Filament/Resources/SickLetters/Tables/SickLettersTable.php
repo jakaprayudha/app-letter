2474bddaedd9b89d64f63f5a8c72cfa9f571155b
jakaprayudha <jakaprayudha3@gmail.com>
2026-03-02 10:57:33 +0700

    feat letter sick print

--- app/Filament/Resources/SickLetters/Tables/SickLettersTable.php
+++ app/Filament/Resources/SickLetters/Tables/SickLettersTable.php
@@ -7,6 +7,7 @@ use Filament\Actions\DeleteBulkAction;
 use Filament\Actions\EditAction;
 use Filament\Tables\Columns\TextColumn;
 use Filament\Tables\Table;
+use Filament\Actions\Action;
 
 class SickLettersTable
 {
@@ -41,6 +42,12 @@ class SickLettersTable
                 //
             ])
             ->recordActions([
+                Action::make('print')
+                    ->label('Print')
+                    ->color('success')
+                    ->icon('heroicon-s-printer')
+                    ->url(fn($record) => route('sick-letter.print', $record))
+                    ->openUrlInNewTab(),
                 EditAction::make(),
             ])
             ->toolbarActions([
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Models\Doctor;
+use App\Models\SickLetter;
 use Barryvdh\DomPDF\Facade\Pdf;
 
 Route::get('/', function () {
@@ -18,3 +19,10 @@ Route::get('doctors/export-pdf', function () {
 
     return $pdf->download('doctors.pdf');
 })->name('doctors.export-pdf');
+
+Route::get('/sick-letter/{record}/print', function (SickLetter $record) {
+    $pdf = Pdf::loadView('pdf.sick-letter', [
+        'data' => $record
+    ]);
+    return $pdf->stream('Surat-Sakit.pdf');
+})->name('sick-letter.print');
