a08fe2c2f94b80c5f0977bb1e9049d373e897132
jakaprayudha <jakaprayudha3@gmail.com>
2026-04-13 10:48:10 +0700

    fix route libray db transaction

--- app/Models/SickLetter.php
+++ app/Models/SickLetter.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 
 class SickLetter extends Model
 {
@@ -23,7 +24,7 @@ class SickLetter extends Model
     {
         return $this->belongsTo(Poli::class);
     }
-    use Illuminate\Support\Facades\DB;
+
 
     protected static function booted()
     {
